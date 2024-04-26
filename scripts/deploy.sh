e98e75b24dd06c6c79113149518c157172d7c201
ByloTonix <bylotonix@gmail.com>
2024-04-26 18:33:55 +0300

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-NAME=fullscreen-to-empty-workspace@aiono.dev
+NAME=fullscreen-to-empty-workspace@revoqaux.dev
 DIR=src
 rm -rf ~/.local/share/gnome-shell/extensions/$NAME
 cp -r src ~/.local/share/gnome-shell/extensions/$NAME
